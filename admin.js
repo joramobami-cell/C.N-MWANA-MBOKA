8cc17d811546faa73c1cf5698450fe7a5808df76
Mwana Mboka <joramobami@gmail.com>
2026-07-02 08:26:34 +0100

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,53 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  set
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDHMovN3CpVl6fQUDZGRNqFu6mLUUPR8Sc",
+  authDomain: "c-n-mwana-mboka.firebaseapp.com",
+  databaseURL: "https://c-n-mwana-mboka-default-rtdb.europe-west1.firebasedatabase.app/",
+  projectId: "c-n-mwana-mboka",
+  storageBucket: "c-n-mwana-mboka.firebasestorage.app",
+  messagingSenderId: "757726608581",
+  appId: "1:757726608581:web:27fa7003ffa955188304ac"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+window.ajouterMembre = async function () {
+
+  const matricule = document.getElementById("matricule").value.trim();
+  const nom = document.getElementById("nom").value.trim();
+  const telephone = document.getElementById("telephone").value.trim();
+  const motdepasse = document.getElementById("motdepasse").value.trim();
+  const statut = document.getElementById("statut").value;
+
+  if (!matricule || !nom || !telephone || !motdepasse) {
+    alert("Veuillez remplir tous les champs.");
+    return;
+  }
+
+  await set(ref(db, "membres/" + matricule), {
+    matricule,
+    nom,
+    telephone,
+    motdepasse,
+    statut,
+    profession: "",
+    adresse: "",
+    parrain: "",
+    dateadhesion: new Date().toLocaleDateString("fr-FR")
+  });
+
+  alert("✅ Membre ajouté avec succès.");
+
+  document.getElementById("matricule").value = "";
+  document.getElementById("nom").value = "";
+  document.getElementById("telephone").value = "";
+  document.getElementById("motdepasse").value = "";
+  document.getElementById("statut").value = "Actif";
+};
